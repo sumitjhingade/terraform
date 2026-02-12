9318de79129ac39c8ef735fc3e470ddfaf1a7ab8
sumitjhingade <127714607+sumitjhingade@users.noreply.github.com>
2026-02-12 17:58:32 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA6ODU2YRDIT4NSKRE"
-  secret_key = "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"
 }
 
 resource "aws_instance" "myec2" {
