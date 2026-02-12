4b46336a4cb40dee5f1bc92b635be471a17d594d
sumitjhingade <127714607+sumitjhingade@users.noreply.github.com>
2026-02-12 18:17:51 +0530

    data block to pull image

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,29 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
+data "aws_ami" "ami_id" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^myami-[0-9]{3}"
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+}
 resource "aws_instance" "myec2" {
-    ami = "ami-00c39f71452c08778"
+    ami = aws_ami.ami_id
     instance_type = "t2.micro"
 }
